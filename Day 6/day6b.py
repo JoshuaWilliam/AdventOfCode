3ecd75541362164478887c2e3e01ee1edaf26fd5
Joshua <joshua_william@hotmail.com>
2020-12-07 06:43:56 +0100

    Completed Day 6 Part Two

--- Day 6/day6b.py
+++ Day 6/day6b.py
@@ -1,7 +1,6 @@
-
 def read_file(filename):
     with open(filename) as f:
-        return [group.replace('\n', '') for group in f.read().split('\n\n')]
+        return [group.split('\n') for group in f.read().split('\n\n')]
 
 
 def get_count_in_group(group):
@@ -13,6 +12,23 @@ def get_counts(groups):
     return sum([get_count_in_group(group) for group in groups])
 
 
+def get_unanimous_counts(groups):
+    unique_questions = dict((letter, 0) for letter in set(''.join(el for el in groups)))
+    counts = 0
+
+    for group in groups:
+        for letter in group:
+            unique_questions[letter] += group.count(letter)
+            if unique_questions[letter] == len(groups):
+                counts += 1
+    return counts
+
+
+def get_total_unanimous(all_groups):
+    return sum([get_unanimous_counts(groups) for groups in all_groups])
+
+
 if __name__ == '__main__':
-    data = read_file('input.txt')
-    print(get_counts(data))
+    data = read_file('test.txt')
+    print(get_total_unanimous(data))
+
